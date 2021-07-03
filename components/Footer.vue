9ef26c4b2ae9e85d7d0856109ada65009b79a497
AJ-Wi <tropaguararia28@gmail.com>
2021-07-03 08:47:23 -0500

    creando header y footer base

---
+++ components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer><h1>pie</h1></footer>
+</template>
+
+<style lang="stylus">
+footer
+  height 200px
+  background-color black
+  border-bottom 1px solid red
+</style>
--- components/Header.vue
+++ components/Header.vue
@@ -1,1 +1,10 @@
-<template> </template>
+<template>
+  <header></header>
+</template>
+
+<style lang="stylus">
+header
+  height 80px
+  background-color red
+  border-bottom 1px solid black
+</style>
