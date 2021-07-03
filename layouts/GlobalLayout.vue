95af6fa2ff875cdb97b825f7bbf6d3fcde5ecbf4
AJ-Wi <tropaguararia28@gmail.com>
2021-07-03 08:47:07 -0500

    estructrura base

---
+++ layouts/GlobalLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="site-container">
+    <Header
+      :is-open="isMobileHeaderOpen"
+      @toggle-sidebar="isMobileHeaderOpen = !isMobileHeaderOpen"
+    />
+    <DefaultGlobalLayout
+      class="site-content"
+      @click="isMobileHeaderOpen = false"
+    />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import GlobalLayout from "@app/components/GlobalLayout.vue";
+import Header from "@theme/components/Header.vue";
+import Footer from "@theme/components/Footer.vue";
+
+export default {
+  components: {
+    DefaultGlobalLayout: GlobalLayout,
+    Header,
+    Footer,
+  },
+
+  data() {
+    return {
+      isMobileHeaderOpen: false,
+    };
+  },
+
+  mounted() {
+    this.$router.afterEach(() => {
+      this.isMobileHeaderOpen = false;
+    });
+  },
+};
+</script>
+
+<style lang="stylus"></style>
---
+++ layouts/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Content />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<style lang="stylus"></style>
