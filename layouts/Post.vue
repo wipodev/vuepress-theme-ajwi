e16b9cf4b6c4f65639aa654d1466838d66214ff9
AJ-Wi <tropaguararia28@gmail.com>
2021-07-04 11:45:25 -0500

    creado layout para post

---
+++ layouts/Post.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="site-post">
+    <article class="post" itemscope itemtype="https://schema.org/BlogPosting">
+      <header>
+        <h1 class="post-title" itemprop="name headline">
+          {{ $frontmatter.title }}
+        </h1>
+        <PostMeta
+          :tags="$frontmatter.tags"
+          :author="$frontmatter.author"
+          :date="$frontmatter.date"
+          :location="$frontmatter.location"
+        />
+      </header>
+      <Content itemprop="articleBody" />
+    </article>
+  </div>
+</template>
+
+<script>
+import PostMeta from "@theme/components/PostMeta.vue";
+
+export default {
+  components: {
+    PostMeta,
+  },
+};
+</script>
+
+<style lang="stylus">
+#site-post
+  width 100vw
+  padding 15px
+
+  .post
+    background-color $bgPosts
+    border-radius 3px
+    padding 15px
+</style>
