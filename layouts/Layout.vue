8f26b6a372fd4a3b6fbce30114dd4a9ad5ab2745
AJ-Wi <tropaguararia28@gmail.com>
2021-07-04 11:45:52 -0500

    agregado cover y suscribe al diseño

--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
+    <Cover v-if="$page.path === '/'" />
     <BaseListPosts v-if="$pagination" />
     <Content v-else />
+    <Suscribe v-if="$page.path === '/'" />
   </div>
 </template>
+
+<script>
+import Cover from "@theme/components/Cover.vue";
+import Suscribe from "@theme/components/Suscribe.vue";
+
+export default {
+  components: {
+    Cover,
+    Suscribe,
+  },
+};
+</script>
