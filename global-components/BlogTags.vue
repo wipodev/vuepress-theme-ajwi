d175181649dfb340ae6f48ecc2b2a800e6d70853
AJ-Wi <tropaguararia28@gmail.com>
2021-07-16 11:03:18 -0500

    agregado etiquetas

---
+++ global-components/BlogTags.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="blog-tags">
+    <BlogTag v-for="tag in tags" :key="tag.name" :tag="tag" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tags: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
+</script>
+
+<style lang="stylus">
+.blog-tags
+  width 100%
+  padding 15px
+
+@media screen and (max-width: 1000px)
+  .blog-tags
+    width 90%
+</style>
