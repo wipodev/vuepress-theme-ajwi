e6159c8657305d1443d7cfb386ae3390b8b25ae7
wipodev <ajwipo@gmail.com>
2023-02-25 20:40:36 -0500

    adding home page and settings

---
+++ doc/.vitepress/config.js
@@ -0,0 +1,84 @@
+export default {
+  title: 'WipoDev',
+  titleTemplate: 'WipoDev | :title',
+  description: 'Custom theme to get a blog using VuePress.',
+  lastUpdated: true,
+  base: '/vuepress-theme-ajwi/',
+  head: [
+    [
+      'link',
+      { rel: 'icon', href: '/vuepress-theme-ajwi/logo.svg', type: 'image/svg' },
+    ],
+  ],
+  themeConfig: {
+    siteTitle: '',
+    logo: 'assets/img/logo-main.svg',
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/wipodev/vuepress-theme-ajwi/',
+      },
+    ],
+  },
+  locales: {
+    root: {
+      label: 'English',
+      lan: 'en',
+      themeConfig: {
+        nav: [
+          { text: 'Home', link: 'https://wipodev.com' },
+          { text: 'VuePress Theme', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          { text: 'Config', link: '/config/' },
+        ],
+        sidebar: {
+          '/config/': [
+            {
+              text: 'Config',
+              collapsed: false,
+              items: [
+                { text: 'Front Matter', link: '/config/front-matter' },
+                { text: 'Palette', link: '/config/palette' },
+              ],
+            },
+          ],
+        },
+        footer: {
+          message: 'Released under the MIT License.',
+          copyright:
+            'Copyright © 2023-present <a href="https://github.com/wipodev">WipoDev</a>',
+        },
+      },
+    },
+    es: {
+      label: 'Spanish',
+      lan: 'es',
+      link: '/es/',
+      themeConfig: {
+        nav: [
+          { text: 'Inicio', link: 'https://wipodev.com/es/' },
+          { text: 'Tema VuePress', link: '/es/' },
+          { text: 'Guía', link: '/es/guide/' },
+          { text: 'Configuración', link: '/es/config/' },
+        ],
+        sidebar: {
+          '/es/config/': [
+            {
+              text: 'Configuración',
+              collapsed: false,
+              items: [
+                { text: 'Front Matter', link: '/es/config/front-matter' },
+                { text: 'Palette', link: '/es/config/palette' },
+              ],
+            },
+          ],
+        },
+        footer: {
+          message: 'Released under the MIT License.',
+          copyright:
+            'Copyright © 2023-presente <a href="https://github.com/wipodev">WipoDev</a>',
+        },
+      },
+    },
+  },
+}
---
+++ doc/index.md
@@ -0,0 +1,24 @@
+---
+layout: home
+title: VuePress Theme AJWi
+hero:
+  name: Custom theme to get a blog using VuePress.
+  tagline: In this guide, you will learn how to add the theme to a new project manually.
+  image:
+    src: /assets/img/logo.svg
+    alt: WipoDev
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /Guide/
+features:
+  - icon: i
+    title: Install
+    details: $ npm install vuepress-theme-ajwi --save-dev
+  - icon: 🛠️
+    title: Config
+    details: blog configuration file .vuepress/config.js
+  - icon: p
+    title: post
+    details: The \_posts folder is where your blog posts are located. You can simply write blog posts in Markdown.
+---
